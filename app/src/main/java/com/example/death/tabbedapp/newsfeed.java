3e51db2e826cab053ef9b0ea3cef482b067b6eb6
Death <usaid87243>
2018-01-08 00:45:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/death/tabbedapp/newsfeed.java
@@ -0,0 +1,23 @@
+package com.example.death.tabbedapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Death on 12/21/2017.
+ */
+
+public class newsfeed extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View myview=inflater.inflate(R.layout.newsfeed,null);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
